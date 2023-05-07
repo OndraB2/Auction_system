180e1339fa962358ef0c5280cbb7b426ece1f155
AdamZaviral <zaviral.adam@gmail.com>
2023-05-07 21:43:23 +0100

    merge

--- AuctionServer/Auction.cs
+++ AuctionServer/Auction.cs
@@ -23,17 +23,61 @@ namespace AuctionServer{
             observers.Add(observer);		
         }
 
-        public void NotifyAllObserversAboutNewBid(){
-            foreach (KademliaNode observer in observers) {
-                observer.UpdateNewBid();
+        public void NotifyAllObserversAboutNewBid(Transaction transaction){
+            foreach (User observer in observers) {
+                observer.UpdateNewBid(transaction);
             }
         } 
 
-        public void NotifyAllObserversAboutEndOfAuction(){
-            foreach (KademliaNode observer in observers) {
-                observer.UpdateEndOfAcution();
+        public void NotifyAllObserversAboutEndOfAuction(Transaction transaction){
+            foreach (User observer in observers) {
+                observer.UpdateEndOfAcution(transaction);
             }
         } 		
+
+        public void GenerateNew()
+        {
+            AuctionTransaction = NewAuctionItemTransaction.GetRandom();
+
+            HighestBid = 0;
+            HighestBidderID = BitConverter.GetBytes(0);
+            StartingBid = AuctionTransaction.GetStartingBid();
+            FinalBid = AuctionTransaction.GetFinalBid();
+            observers = new List<User>();
+        }
+
+        public void NewBid(NewItemBidTransaction NIBtransaction)
+        {
+            if( NIBtransaction.Amount > HighestBid && NIBtransaction.Amount > StartingBid)
+            {
+                HighestBid = NIBtransaction.Amount;
+                HighestBidderID = NIBtransaction.TransactionOwnerId;
+                TransactionPool.AddTransactionToPool(NIBtransaction);
+                System.Console.WriteLine("\tNew highest bid by: , value: " + NIBtransaction.Amount);
+                NotifyAllObserversAboutNewBid(NIBtransaction);
+            }
+            if( NIBtransaction.Amount >= HighestBid && NIBtransaction.Amount > FinalBid )
+            {
+                HighestBid = NIBtransaction.Amount;
+                HighestBidderID = NIBtransaction.TransactionOwnerId;
+                EndOfAuctionTransaction EOATransaction = EndOfAuctionTransaction.CreateNew(AuctionTransaction.AuctionItemId, AuctionTransaction.AuctionOwnerId, NIBtransaction.Amount, NIBtransaction.TransactionOwnerId);
+                EndAuction(EOATransaction);
+            }
+        }
+        public void EndOfAuctionByOwner(EndOfAuctionTransaction EOATransaction)
+        {
+            if( EOATransaction.TransactionOwnerId == AuctionTransaction.AuctionOwnerId)
+            {
+                EndAuction(EOATransaction);
+            }
+        }
+        public void EndAuction(EndOfAuctionTransaction EOATransaction)
+        {
+            TransactionPool.AddTransactionToPool(EOATransaction);    
+            System.Console.WriteLine("End, Sold to: " + new Guid(EOATransaction.TransactionOwnerId) + ", sold for: " + EOATransaction.Amount + "\n");
+            ActiveAuctions.RemoveAuction(AuctionTransaction);
+            NotifyAllObserversAboutEndOfAuction(EOATransaction);
+        }
 
         public void NewBid(double amount, Byte[] bidderID)
         {
